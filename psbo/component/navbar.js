1bb8358ec67ef2522e8520636667cacbcd0894cd
blackmanjack <faiz_m17@apps.ipb.ac.id>
2021-06-12 18:18:48 +0700

    :heavy_check_mark: route

--- psbo/component/navbar.js
+++ psbo/component/navbar.js
@@ -1,17 +1,58 @@
-import Link from 'next/link'
-import Image from 'next/image'
+import Link from "next/link";
+import Image from "next/image";
+import { useContext } from "react";
+import Cookies from "js-cookie";
 
 const Navbar = () => {
-    return(
-        <nav className="font-sans">
-            <div className="logo">
-                <Image src="/ipbscholar.png" width={150} height={60} />
-            </div>
-            <Link className="font-sans" href="/login"><a>Login</a></Link>
-            <Link className="font-sans" href="/"><a>Home</a></Link>
-            <Link className="font-sans" href="/profile"><a>Profile</a></Link>
-        </nav>
-    )
-}
+  const handleLogout = () => {
+    Cookies.remove("token");
+    Cookies.remove("name");
+    Cookies.remove("nim");
+    // setUser(null);
+    localStorage.clear();
+
+    return (
+      <>
+        <Link className="font-sans" href="/home">
+          <a>Home</a>
+        </Link>
+        <Link className="font-sans" href="/profile">
+          <a>Profile</a>
+        </Link>
+        <Link className="font-sans" href="/login">
+          <a onClick={handleLogout}>Logout</a>
+        </Link>
+      </>
+    );
+  };
+
+  return (
+    <nav className="font-sans">
+      <div className="logo">
+        <Image src="/ipbscholar.png" width={150} height={60} />
+      </div>
+
+      {Cookies.get("token") === null || Cookies.get("token") === undefined ? (
+        <>
+          <Link className="font-sans" href="/login">
+            <a>Login</a>
+          </Link>
+        </>
+      ) : (
+        <>
+          <Link className="font-sans" href="/home">
+            <a>Home</a>
+          </Link>
+          <Link className="font-sans" href="/profile">
+            <a>Profile</a>
+          </Link>
+          <Link className="font-sans" href="/login">
+            <a onClick={handleLogout}>Logout</a>
+          </Link>
+        </>
+      )}
+    </nav>
+  );
+};
 
 export default Navbar;
--- psbo/pages/_app.js
+++ psbo/pages/_app.js
@@ -1,14 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 import "@material-tailwind/react/tailwind.css";
-import 'tailwindcss/tailwind.css'
-import Layout from '../component/layout'
+import "tailwindcss/tailwind.css";
+import Router from "next/router";
+import Layout from "../component/layout";
 
 function MyApp({ Component, pageProps }) {
   return (
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-  )
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
